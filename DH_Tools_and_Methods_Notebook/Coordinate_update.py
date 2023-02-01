c8371eb2053f1977632bd1228a2465e724bb5ec3
Nicole Bausch <arcanahereticae13@gmail.com>
2023-02-01 22:57:56 +0100

    What script should look like

--- DH_Tools_and_Methods_Notebook/Coordinate_update.py
+++ DH_Tools_and_Methods_Notebook/Coordinate_update.py
@@ -1,6 +1,29 @@
-import os
+import pandas as pd
+import geopy.geocoders as gg
+import os 
 
-open("St_Marxer_Spreadsheet.csv", w)
 
-//Check for updates
-//Update Row with new value from Geopandas Notebook
+geolocator = gg.Nominatim(user_agent="your email or app name goes here")
+new_address = input("Please enter the address you would like to check: ")
+location = geolocator.geocode(new_address)
+new_lat = location.latitude
+new_long = location.longitude 
+print(new_lat, new_long)
+
+i = int(input("Which row is this going to?"))
+
+
+def convertLat(i):
+    df = pd.read_csv("St_Marxer_Spreadsheet.csv", sep = ";")
+    df.loc[i, 'decimal(8,6) latitude'] = new_lat
+    df.to_csv("St_Marxer_Spreadsheet.csv")
+    print(new_lat)
+
+def convertLong(i): 
+    df = pd.read_csv("St_Marxer_Spreadsheet.csv", sep = ";")
+    df.loc[i, 'decimal(9,6) longitude'] = new_long
+    df.to_csv("St_Marxer_Spreadsheet.csv")
+    print(new_long)
+        
+convertLat(i)
+convertLong(i)
